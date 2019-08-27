799dc13b2fc3f1a91fe581762bdf90bff960e82a
BLANK-TECH <ivonne_1412@hotmail.com>
2019-08-27 18:53:00 -0500

    lectura de archivo README.md con fs

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const path = require('path');
+
+console.log(path);
+  fs.readFile('./README.md','utf-8',(err,data)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(data);
+    }
+    
+  });
+  
